36f258afe73ae068361e88fdfb1784f3ae27b243
Nimira43 <n.rabone72@aol.com>
2024-10-22 18:09:16 +0100

    Completed - writing a very basic server in pure Node.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   const url = req.url
   const method = req.method
-
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter Message</title></head')
@@ -12,7 +11,6 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()    
   }
-
   if (url == '/message' && method == 'POST') {
     const body = []
     req.on('data', (chunk) => {
@@ -29,7 +27,6 @@ const server = http.createServer((req, res) => {
       }) 
     })    
   }
-
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
   res.write('<head><title>Node App V1.0.5</title></head')
