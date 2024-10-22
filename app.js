37be92961d5adfe82d4a0f96a90a54c2c50c27c7
Nimira43 <n.rabone72@aol.com>
2024-10-22 17:58:30 +0100

    Moved res.statusCode = 302   res.setHeader('Location', '/')       return res.end() into req.on('end')  - This will make this call back function not be called ever. Line 32 and after will be called - event driven code execution

--- app.js
+++ app.js
@@ -23,15 +23,15 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString()
       const message = parsedBody.split('=')[1]
       fs.writeFileSync('message.txt', message) 
-    })
-    res.statusCode = 302
-    res.setHeader('Location', '/')
-    return res.end()
+      res.statusCode = 302
+      res.setHeader('Location', '/')
+      return res.end()
+    })    
   }
 
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
-  res.write('<head><title>Node App V1.0.3</title></head')
+  res.write('<head><title>Node App V1.0.4</title></head')
   res.write('<body><h1>Node App Server</h1></body>')
   res.write('</html>')
   res.end()
