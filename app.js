a652de430312dc51cbc97e6502882a8d802d220b
Nimira43 <n.rabone72@aol.com>
2024-10-22 22:14:20 +0100

    Making middleware always run

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.use('/', (req, res, next) => {
+  console.log('Store always open!')
+  next()
+})
+
 app.use('/add-product', (req, res, next) => {
   console.log('Middleware 1')
   res.send('<h1>"Add Product"</h1>')
