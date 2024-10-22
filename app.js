84c8c015994fc2fbaa783191d13374b2b8697185
Nimira43 <n.rabone72@aol.com>
2024-10-22 17:48:05 +0100

    Parsing request bodies - sending submitted message to txt file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   const url = req.url
   const method = req.method
+
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter Message</title></head')
@@ -11,8 +12,18 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()    
   }
+
   if (url == '/message' && method == 'POST') {
-    fs.writeFileSync('message.txt', 'Message Text')
+    const body = []
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      const message = parsedBody.split('=')[1]
+      fs.writeFileSync('message.txt', message) 
+    })
     res.statusCode = 302
     res.setHeader('Location', '/')
     return res.end()
