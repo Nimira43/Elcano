6a223e6e5fa51930625e984dcea948feec5d6769
Nimira43 <n.rabone72@aol.com>
2024-10-22 17:33:22 +0100

    Redirecting requests and writing to a txt file

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const http = require('http')
+const fs = require('fs') 
 
 const server = http.createServer((req, res) => {
   const url = req.url
+  const method = req.method
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter Message</title></head')
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
     res.write('</html>')
-    return res.end()  
+    return res.end()    
   }
+  if (url == '/message' && method == 'POST') {
+    fs.writeFileSync('message.txt', 'Message Text')
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
+
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
-  res.write('<head><title>Node App V1.0.2</title></head')
+  res.write('<head><title>Node App V1.0.3</title></head')
   res.write('<body><h1>Node App Server</h1></body>')
   res.write('</html>')
   res.end()
