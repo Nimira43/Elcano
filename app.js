8362c05c638fa2df1f3c4e30b2dfd133c23e5e12
Nimira43 <n.rabone72@aol.com>
2024-10-22 21:07:34 +0100

    Testing middleware

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require('express')
 
 const app = express()
 
-const server = http.createServer()
+app.use((req, res, next) => {
+  console.log('In Middleware')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('In Middleware Again')
+})
+
+const server = http.createServer(app)
 
 server.listen(3000)
