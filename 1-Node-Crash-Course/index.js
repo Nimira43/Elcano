506014f1ffba4b840b3bb3575200e9944f74e8b5
Nimira43 <n.rabone72@aol.com>
2024-11-24 06:34:52 +0000

    Getting a random post

--- 1-Node-Crash-Course/index.js
+++ 1-Node-Crash-Course/index.js
@@ -2,5 +2,8 @@ import getPosts, { getPostsLength } from './postController.js'
 import generateRandomNumber from './utils.js'
 
 console.log(getPosts())
+
 console.log('Number of posts:', getPostsLength())
 console.log('A random number:', generateRandomNumber())
+const randomIndex = generateRandomNumber() % getPostsLength()
+console.log('A random post:', getPosts()[randomIndex])
