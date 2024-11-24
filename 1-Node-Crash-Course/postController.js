39306163d9d29be1ada7b2876d0156183b93622d
Nimira43 <n.rabone72@aol.com>
2024-11-24 06:01:45 +0000

    Started crash course by created a random number generator utility and an array of objects containing blog posts.

---
+++ 1-Node-Crash-Course/postController.js
@@ -0,0 +1,51 @@
+const post = [
+  { 
+    id: 1, 
+    title: 'Hamilton joins Ferrari for 2025.', 
+    author: 'Joe Bloggs'
+  },
+  { 
+    id: 2, 
+    title: 'McLaren back to winning ways.', 
+    author: 'Jane Doe'
+  },
+  { 
+    id: 3, 
+    title: 'Will Audi be competitive on debut?', 
+    author: 'John Smith'
+  },
+  { 
+    id: 4, 
+    title: 'Colapinto set to join Red Bull family.', 
+    author: 'Bob Jones'
+  },
+  { id: 5, 
+    title: 'Bottas to joint Mercedes as third driver.', 
+    author: 'Lisa Roberts'
+  },
+  { 
+    id: 6, 
+    title: 'Alpine\'s revival continues in Las Vegas.', 
+    author: 'Fred Hughes'
+  },
+  { 
+    id: 7, 
+    title: 'What is the future hold for Sergio Perez?', 
+    author: 'Mark Porter'
+  },
+  { 
+    id: 8, 
+    title: 'Verstappen on the brink of a fourth world title.', 
+    author: 'Emma Cooper'
+  },
+  { 
+    id: 9, 
+    title: 'Assessing the performance of Lance Stroll.', 
+    author: 'Phil Mann'
+  },
+  { 
+    id: 10, 
+    title: 'What\'s new in Formula 1 for 2025.', 
+    author: 'Joe Bloggs'
+  },
+]
