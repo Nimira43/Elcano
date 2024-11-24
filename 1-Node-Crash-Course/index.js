02c2b3f37183c080e4b5c68070e36f1b829d8bfb
Nimira43 <n.rabone72@aol.com>
2024-11-24 06:15:31 +0000

    Included generateRandomNumber()

--- 1-Node-Crash-Course/index.js
+++ 1-Node-Crash-Course/index.js
@@ -1,4 +1,6 @@
 import getPosts, { getPostsLength } from './postController.js'
+import generateRandomNumber from './utils.js'
 
 console.log(getPosts())
 console.log('Number of posts:', getPostsLength())
+console.log('A random number:', generateRandomNumber())
--- 1-Node-Crash-Course/utils.js
+++ 1-Node-Crash-Course/utils.js
@@ -1,3 +1,5 @@
 function generateRandomNumber() {
   return Math.floor(Math.random() * 100) + 1
 }
+
+export default generateRandomNumber
