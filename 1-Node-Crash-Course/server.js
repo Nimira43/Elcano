980462d6d9fc9b168bda8b8c7a4007786b6febd0
Nimira43 <n.rabone72@aol.com>
2024-12-15 18:38:20 +0000

    Server running..

--- 1-Node-Crash-Course/server.js
+++ 1-Node-Crash-Course/server.js
@@ -1,6 +1,11 @@
 import http from 'http'
 
+const PORT = 8000
 const server = http.createServer((req, res) => {
   res.write('Testing')
   res.end()
+})
+
+server.listen(8000, () => {
+  console.log(`Server running on Port ${PORT}`)
 })
