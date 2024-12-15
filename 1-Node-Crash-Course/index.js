97ffda476988804dec121d9c6e565d91fa99d1b8
Nimira43 <n.rabone72@aol.com>
2024-12-15 18:32:03 +0000

    Starting work using the http module

--- 1-Node-Crash-Course/index.js
+++
@@ -1,9 +0,0 @@
-import getPosts, { getPostsLength } from './postController.js'
-import generateRandomNumber from './utils.js'
-
-console.log(getPosts())
-
-console.log('Number of posts:', getPostsLength())
-console.log('A random number:', generateRandomNumber())
-const randomIndex = generateRandomNumber() % getPostsLength()
-console.log('A random post:', getPosts()[randomIndex])
