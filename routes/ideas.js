48a06688a79be967592395f9b7469fc59eee310a
Nimira43 <n.rabone72@aol.com>
2025-01-31 22:10:17 +0000

    Refactor - server.js - ideasRouter handles /api/deas route. Ideas.js - set up routes to get ideas and specific idea by id.

---
+++ routes/ideas.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const router = express.Router()
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Develop an AI-driven chatbot for customer support.',
+    tag: 'AI',
+    username: 'MaxMac831',
+    date: '2025-01-14'
+  },
+  {
+    id: 2,
+    text: 'Create a responsive portfolio website using React.',
+    tag: 'Frontend',
+    username: 'Leonardo21',
+    date: '2025-01-09'
+  },
+  {
+    id: 3,
+    text: 'Build a RESTful API for an e-commerce platform.',
+    tag: 'Backend',
+    username: 'TalGal_X2',
+    date: '2024-01-28'
+  },
+  {
+    id: 4,
+    text: 'Design a smart home system controlled via mobile app.',
+    tag: 'Internet of Things',
+    username: 'Nimiratech',
+    date: '2025-01-29'
+  },
+  {
+    id: 5,
+    text: 'Implement user authentication and authorisation using Clerk.',
+    tag: 'Backend',
+    username: 'GemmaAI2007',
+    date: '2025-01-14'
+  }
+]
+
+router.get('/', (req, res) => {
+  res.json({
+    success: true,
+    data: ideas
+  })
+})
+
+router.get('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
+
+  if (!idea) {
+    return res.status(404).json({
+      success: false,
+      error: 'Resource not found'
+    })
+  }
+  
+  res.json({
+    success: true,
+    data: idea
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,70 +3,12 @@ const port = 5001
 
 const app = express()
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Develop an AI-driven chatbot for customer support.',
-    tag: 'AI',
-    username: 'MaxMac831',
-    date: '2025-01-14'
-  },
-  {
-    id: 2,
-    text: 'Create a responsive portfolio website using React.',
-    tag: 'Frontend',
-    username: 'Leonardo21',
-    date: '2025-01-09'
-  },
-  {
-    id: 3,
-    text: 'Build a RESTful API for an e-commerce platform.',
-    tag: 'Backend',
-    username: 'TalGal_X2',
-    date: '2024-01-28'
-  },
-  {
-    id: 4,
-    text: 'Design a smart home system controlled via mobile app.',
-    tag: 'Internet of Things',
-    username: 'Nimiratech',
-    date: '2025-01-29'
-  },
-  {
-    id: 5,
-    text: 'Implement user authentication and authorisation using Clerk.',
-    tag: 'Backend',
-    username: 'GemmaAI2007',
-    date: '2025-01-14'
-  }
-];
-
 app.get('/', (req, res) => {
   res.json('Elano Random Ideas Application')
 })
 
-app.get('/api/ideas', (req, res) => {
-  res.json({
-    success: true,
-    data: ideas
-  })
-})
-
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id)
-
-  if (!idea) {
-    return res.status(404).json({
-      success: false,
-      error: 'Resource not found'
-    })
-  }
-  
-  res.json({
-    success: true,
-    data: idea
-  })
-})
+const ideasRouter = require('./routes/ideas')
+app.use('/api/ideas', ideasRouter)
 
 app.listen(port, () => {
   console.log(`Server listening on Port ${port}`)
