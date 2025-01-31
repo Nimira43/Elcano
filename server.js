46401becf40d8672fc26e5a77d3484db0e4cdead
Nimira43 <n.rabone72@aol.com>
2025-01-31 21:19:07 +0000

    Created some dummy data.

--- server.js
+++ server.js
@@ -3,6 +3,47 @@ const port = 5001
 
 const app = express()
 
+const ideas = [
+  {
+    id: 1,
+    text: 'Develop an AI-driven chatbot for customer support.',
+    tag: 'AI',
+    username: 'MaxMac831',
+    date: '2025-01-14'
+  },
+  {
+    id: 2,
+    text: 'Create a responsive portfolio website using React.',
+    tag: 'Frontend',
+    username: 'Leonardo21',
+    date: '2025-01-09'
+  },
+  {
+    id: 3,
+    text: 'Build a RESTful API for an e-commerce platform.',
+    tag: 'Backend',
+    username: 'TalGal_X2',
+    date: '2024-01-28'
+  },
+  {
+    id: 4,
+    text: 'Design a smart home system controlled via mobile app.',
+    tag: 'Internet of Things',
+    username: 'Nimiratech',
+    date: '2025-01-29'
+  },
+  {
+    id: 5,
+    text: 'Implement user authentication and authorisation using Clerk.',
+    tag: 'Backend',
+    username: 'GemmaAI2007',
+    date: '2025-01-14'
+  }
+];
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Elano Random Ideas Application')
 })
