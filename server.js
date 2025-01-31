82b64c6e561c358c73b2ee488167c0d40274d429
Nimira43 <n.rabone72@aol.com>
2025-01-31 22:22:29 +0000

    With the use of express.json() and express.urlencoded({ extended: false }), your server can now handle incoming JSON and URL-encoded data - helps with handling POST requests.

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const port = 5001
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+}))
+
 app.get('/', (req, res) => {
   res.json('Elano Random Ideas Application')
 })
