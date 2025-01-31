00326755abae04478b8bb8d14e7116f9dec225cf
Nimira43 <n.rabone72@aol.com>
2025-01-31 19:55:04 +0000

    Setting up test post request

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req,res) => {
-  res.status(200).send('Server side is operating.')
+  res.status(200).json({
+    message: 'Server side is operating.',
+    app: 'Elcano'
+  })
+})
+
+app.post('/', (req, res) => {
+  res.end('This is where you can post to this endpoint...')
 })
 
 app.listen(port, () => {
-  console.log(`Server running on port ${3000}`)
+  console.log(`Server running on port ${port}`)
 }) 
