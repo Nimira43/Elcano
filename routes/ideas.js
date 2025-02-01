3aa6665427c2c4ce6e5d7512aa5cd51c00868e11
Nimira43 <n.rabone72@aol.com>
2025-02-01 09:09:42 +0000

    Adding a PUT route to update an idea.

--- routes/ideas.js
+++ routes/ideas.js
@@ -78,4 +78,23 @@ router.post('/', (req, res) => {
   })
 })
 
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
+
+  if (!idea) {
+    return res.status(404).json({
+      success: false,
+      error: 'Resource not found'
+    })
+  }
+  
+  idea.text = req.body.text || idea.text
+  idea.tag = req.body.tag || idea.tag
+
+  res.json({
+    success: true,
+    data: idea
+  })
+})
+
 module.exports = router
