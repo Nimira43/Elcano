ab8f5fa0a3fe6f3319c16dc666bc09d45eb3e61f
Nimira43 <n.rabone72@aol.com>
2025-06-10 18:45:57 +0100

    Setting up container. Setting up various bg colours.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,16 @@ export default {
         logo: ['Loved by the King', 'cursive']
       },
       colors: {
-        'main': '#ff4500',
-        'support': '00ffff',
         'dark': '#111',
         'light': '#fffcfa',
         'grey-dark': '#333',
-        'grey-medium': '#999',
         'grey-light': '#ccc',
         'grey-light-extra': '#eee',
+        'green-bg': '#39e02d',
+        'blue-bg': '#0c69dc',
+        'red-bg': '#0c69dc',
+        'orange-bg': '#ff4500',
+        'yellow-bg': '#c9ac0b'
       }
      },
    },
